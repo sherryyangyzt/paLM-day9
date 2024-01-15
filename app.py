0e66289f43d41cc5eeeea7d6131d5d472df17334
sherryyangyzt <142111351+sherryyangyzt@users.noreply.github.com>
2024-01-15 16:09:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-
+from flask import Flask, request, render_template
+import google.generativeai as palm
+palm.configure(api_key="AIzaSyATJoqfdX8aUByPt-UC-OI8LQeijb8k2K4")
+model = {"model":"models/chat-bison-001"}
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("q")
+        print(q)
+        r = palm.chat(
+            **model,
+            messages = q
+        )
+        return(render_template("index.html",result=r.last))
+    else:
+        return(render_template("index.html", result="waiting for question............."))
+if __name__ == "__main__":
+    app.run()
